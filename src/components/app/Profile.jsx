53d75a93245bf1f359f7e2001b1b491d2f35f2bd
Varenic Victor <victorv@touchpanelcontrol.com>
2018-06-21 13:44:22 +0300

    profile

--- src/components/app/Profile.jsx
+++ src/components/app/Profile.jsx
@@ -5,8 +5,15 @@ class Profile extends Component {
     render() {
         return (
             <div className="profile">
+                <div className="profileUser__status">Online</div>
                 <div className="profileUser__name">{new Chance().name()}</div>
-                <div className="profileUser__status">{new Chance().sentence()}</div>
+                <div className="profileUser__quote">{new Chance().sentence()}</div>
+                <div className="line"></div>
+                <div>Birthday: </div>
+                <div>Hometown: </div>
+                <div>Relationship status: </div>
+                <div>Company: </div>
+                <div>Language: </div>
                 <div className="line"></div>
             </div>
         );
