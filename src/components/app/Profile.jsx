9fef128872b360f7814cabcbe265c632b05bb9a5
Varenic Victor <victorv@touchpanelcontrol.com>
2018-06-21 13:10:40 +0300

    profile

--- src/components/app/Profile.jsx
+++ src/components/app/Profile.jsx
@@ -1,10 +1,13 @@
 import React, {Component} from "react";
+import * as Chance from 'chance';
 
 class Profile extends Component {
     render() {
         return (
             <div className="profile">
-                
+                <div className="profileUser__name">{new Chance().name()}</div>
+                <div className="profileUser__status">{new Chance().sentence()}</div>
+                <div className="line"></div>
             </div>
         );
     }
