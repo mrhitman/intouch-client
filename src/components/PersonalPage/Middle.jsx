2fdad7b1858341ce7f3266e5e311ba11d9fa619b
Varenic Victor <victorv@touchpanelcontrol.com>
2018-07-03 16:59:37 +0300

    modal

--- src/components/PersonalPage/Middle.jsx
+++ src/components/PersonalPage/Middle.jsx
@@ -1,13 +1,38 @@
 import React, { Component } from "react";
+import Modal from 'react-modal';
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
+const customStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)'
+  }
+};
+
 class Middle extends Component {
+  state = {
+    modalIsOpen: false
+  };
+
   render() {
     return (
       <div className="middle">
         <div className="photo">
-          <img src="photo.jpg" alt="" />
+          <img src="photo.jpg" alt="" onClick={this.openModal}/>
+          <Modal
+            isOpen={this.state.modalIsOpen}
+            // onAfterOpen={this.afterOpenModal}
+            onRequestClose={this.closeModal}
+            style={customStyles}
+            contentLabel="Example Modal"
+          >
+            <img src="photo.jpg" alt="" onClick={this.openModal}/>
+          </Modal>
         </div>
         <div className="sendButton">Send a message</div>
         <div className="info">Unknown is your friend</div>
@@ -22,6 +47,14 @@ class Middle extends Component {
         <div className="line" />
       </div>
     );
+  }
+
+  openModal = () => {
+    this.setState({modalIsOpen: true});
+  }
+
+  closeModal = () => {
+    this.setState({modalIsOpen: false});
   }
 
   renderPeople(people) {
