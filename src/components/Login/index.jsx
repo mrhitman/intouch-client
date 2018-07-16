77f62089b48d3d2f4d5460790a87e61e7261cc94
Varenic Victor <victorv@touchpanelcontrol.com>
2018-07-16 12:50:11 +0300

    style login

--- src/components/Login/index.jsx
+++ src/components/Login/index.jsx
@@ -1,9 +1,12 @@
 import React, { Component } from "react";
 import Header from '../common/Header';
 import axios from 'axios';
+import '../../styles/login.css';
 
 
-const baseUrl = 'http://localhost:3001';
+const port = 3000;
+const protocol = 'http';
+const host = 'localhost';
 
 class LoginForm extends Component {
 
@@ -15,7 +18,7 @@ class LoginForm extends Component {
 
     onClick = () => {
         const { email, password } = this.state;
-        axios.post(`${baseUrl}/user/login`, { email, password })
+        axios.post(`${protocol}://${host}:${port}/user/login`, { email, password })
             .then(response => this.setState({ userData: response.data }));
     }
 
@@ -41,7 +44,8 @@ class LoginForm extends Component {
                     <div className="login">
                         <input id="email" type="text" placeholder="email" value={email} onChange={this.changeValue} />
                         <input id="password" type="password" placeholder="password" value={password} onChange={this.changeValue} onKeyPress={this.onEnter}/>
-                        <button onClick={this.onClick}>Enter</button>
+                        <button onClick={this.onClick}>Login</button>
+                        <button onClick={this.onClick}>Register</button>
                     </div>
                 </div>
             </div>
