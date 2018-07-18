c001a15fcf32d33df5b243148cfcc69547414921
Varenic Victor <victorv@touchpanelcontrol.com>
2018-07-18 16:32:27 +0300

    -

--- src/components/Registration/index.jsx
+++ src/components/Registration/index.jsx
@@ -1,15 +1,29 @@
 import React, { Component } from 'react';
+import { baseUri } from '../../constats';
 
 class RegistrationForm extends Component {
+    state = {
+        email: '',
+        password: '',
+        repassword: '',
+        loginError: null,
+    }
+
+    changeValue = e => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
 
     render() {
+        const { email, password, repassword, loginError } = this.state;
         return (
-        <form target='' method='POST' onSubmit={console.log}>
-            <input type='text' value='' placeholder='email' />
-            <input type='password' value='' placeholder='password' />
-            <input type='password' value='' placeholder='repeat password' />
-            <button type='submit'>Register</button>
-        </form>
+            <form target={`${baseUri}/user/register`} method='POST' onSubmit={console.log}>
+                <input id='email' type='text' value='' placeholder='email' value={email} onChange={this.changeValue}/>
+                <input id='password' type='password' value='' placeholder='password' value={password} onChange={this.changeValue}/>
+                <input id='repassword' type='password' value='' placeholder='repeat password' value={repassword} onChange={this.changeValue}/>
+                <button type='submit'>Register</button>
+            </form>
         );
     }
 }
