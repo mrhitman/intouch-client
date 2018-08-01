4a0f1bf7a615c6a488127f9ccc8cdc6756614cfc
Varenic Victor <victorv@touchpanelcontrol.com>
2018-08-01 18:59:10 +0300

    update

--- src/components/PersonalPage/Update.jsx
+++ src/components/PersonalPage/Update.jsx
@@ -1,4 +1,4 @@
-import { Button, Checkbox, DatePicker, Col, Form, Icon, Input, Row } from 'antd';
+import { Button, Col, DatePicker, Form, Input, Row, Tabs } from 'antd';
 import React, { Component } from 'react';
 import Layout from '../common/Layout';
 import LeftMenu from '../common/LeftMenu';
@@ -28,27 +28,45 @@ class UpdateProfile extends Component {
                         <LeftMenu />
                     </Col>
                     <Col span={18} style={{ margin: '30px 0 0 10px' }}>
-                        <FormItem label='Name' {...formItemLayout} >
-                            {getFieldDecorator('name', { rules: [{ required: true }] })(<Input />)}
-                        </FormItem>
-                        <FormItem label='Second name' {...formItemLayout}>
-                            {getFieldDecorator('name', { rules: [{ required: true }] })(<Input />)}
-                        </FormItem>
-                        <FormItem label='Birthday' {...formItemLayout}>
-                            {getFieldDecorator('birthday', { rules: [{ required: true }] })(<DatePicker />)}
-                        </FormItem>
-                        <FormItem label='Hometown' {...formItemLayout}>
-                            {getFieldDecorator('hometown', { rules: [{ required: true }] })(<Input />)}
-                        </FormItem>
-                        <FormItem label='Company' {...formItemLayout}>
-                            {getFieldDecorator('Company', { rules: [{ required: true }] })(<Input />)}
-                        </FormItem>
-                        <FormItem wrapperCol={{
-                            xs: { span: 24, offset: 0 },
-                            sm: { span: 14, offset: 4 },
-                        }}>
-                            <Button type="primary" onClick={this.handleSubmit}>Update</Button>
-                        </FormItem>
+                        <Tabs>
+                            <Tabs.TabPane tab="Profile" key={1} >
+                                <FormItem label='Name' {...formItemLayout} >
+                                    {getFieldDecorator('name', { rules: [{ required: true }] })(<Input />)}
+                                </FormItem>
+                                <FormItem label='Second name' {...formItemLayout}>
+                                    {getFieldDecorator('name', { rules: [{ required: true }] })(<Input />)}
+                                </FormItem>
+                                <FormItem label='Birthday' {...formItemLayout}>
+                                    {getFieldDecorator('birthday', { rules: [{ required: true }] })(<DatePicker />)}
+                                </FormItem>
+                                <FormItem label='Hometown' {...formItemLayout}>
+                                    {getFieldDecorator('hometown', { rules: [{ required: true }] })(<Input />)}
+                                </FormItem>
+                                <FormItem label='Company' {...formItemLayout}>
+                                    {getFieldDecorator('Company', { rules: [{ required: true }] })(<Input />)}
+                                </FormItem>
+                                <FormItem wrapperCol={{
+                                    xs: { span: 24, offset: 0 },
+                                    sm: { span: 14, offset: 4 },
+                                }}>
+                                    <Button type="primary" onClick={this.handleSubmit}>Update</Button>
+                                </FormItem>
+                            </Tabs.TabPane>
+                            <Tabs.TabPane tab="Additional" key={2}>
+                                <FormItem label='Hobbies' {...formItemLayout}>
+                                    {getFieldDecorator('hobbies')(<Input.TextArea autosize={{ minRows: 2, maxRows: 6 }} />)}
+                                </FormItem>
+                                <FormItem label='Priorities' {...formItemLayout}>
+                                    {getFieldDecorator('priorities')(<Input.TextArea autosize={{ minRows: 2, maxRows: 6 }} />)}
+                                </FormItem>
+                                <FormItem wrapperCol={{
+                                    xs: { span: 24, offset: 0 },
+                                    sm: { span: 14, offset: 4 },
+                                }}>
+                                    <Button type="primary" onClick={this.handleSubmit}>Update</Button>
+                                </FormItem>
+                            </Tabs.TabPane>
+                        </Tabs>
                     </Col>
                 </Row>
             </Layout >
